19cbcc944665e64680984dfe9cb869ab3fa0c6cd
ArtemMaluev <artem.rus13@yandex.ru>
2022-07-16 17:52:20 +0300

    add feature/query

--- src/main/java/Request.java
+++ src/main/java/Request.java
@@ -1,18 +1,25 @@
-import java.io.InputStream;
-import java.util.Map;
+import org.apache.http.NameValuePair;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Request {
 
     private final String method;
     private final String path;
-    private final Map<String, String> headers;
-    private final InputStream body;
+    private final List<String> headers;
+    private final String body;
+    private List<NameValuePair> queryParams;
 
-    public Request(String method, String path, Map<String, String> headers, InputStream body) {
+    public Request(String method, String path, List<String> headers, String body) {
         this.method = method;
         this.path = path;
         this.headers = headers;
         this.body = body;
+    }
+
+    public void setQueryParams(List<NameValuePair> params) {
+        this.queryParams = params;
     }
 
     public String getMethod() {
@@ -23,19 +30,31 @@ public class Request {
         return path;
     }
 
-    public Map<String, String> getHeaders() {return headers; }
+    public List<String> getHeaders() {return headers; }
 
-    public InputStream getBody() {
+    public String getBody() {
         return body;
+    }
+
+    public List<NameValuePair> getQueryParams() {
+        return queryParams;
+    }
+
+    public String getQueryParam(String param) {
+        return queryParams.stream()
+                .filter(p -> p.getName().equals(param))
+                .map(NameValuePair::getValue)
+                .collect(Collectors.joining(", "));
     }
 
     @Override
     public String toString() {
         return "Request{" +
-                "method='" + method + '\'' +
-                ", path='" + path + '\'' +
-                ", headers=" + headers +
-                ", body='" + body + '\'' +
+                "\n method='" + method + '\'' +
+                "\n path='" + path + '\'' +
+                "\n headers=" + headers +
+                "\n body='" + body + '\'' +
+                "\n queryParams=" + queryParams +
                 '}';
     }
 }
